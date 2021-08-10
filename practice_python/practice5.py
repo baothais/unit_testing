28e68a3d3ff02493d7f53677f1649bad3936902b
baothai <thaibao14h5@gmail.com>
2021-08-10 07:40:53 +0700

    second commit

---
+++ practice_python/practice5.py
@@ -0,0 +1,24 @@
+class Practice5:
+
+    @staticmethod
+    def show_menu():
+        print("PRACTICE 5".center(50, "="))
+        print("1. ")
+        print("0. Exit")
+        print("".center(50, "="))
+
+if __name__=="__main__":
+    p = Practice5()
+    p.show_menu()
+    choose = int(input("Enter your chooses: "))
+    print()
+    while choose != 0:
+        if choose == 1:
+            pass
+
+        if choose == 2:
+            pass
+
+        choose = int(input("\nEnter your chooses: "))
+        print()
+    print("EXIT PRACTICE 5".center(50, "="))
---
+++ practice_python/test_practice5.py
@@ -0,0 +1,10 @@
+from . import practice5
+
+p = practice5.Practice5()
+
+class TestPractice5:
+
+    pass
+
+if __name__=="__main__":
+    pass
